d40ed82c5a12eba1fa4a1269e911ec819322b7a4
chaplinsiva <talkschaplin@gmail.com>
2025-11-10 16:47:32 +0530

    chore: remove Host directive from generated robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,21 @@ module.exports = {
   siteUrl: 'https://celite.in',
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    transformRobotsTxt: async (_config, robotsTxt) =>
+      robotsTxt
+        .split('\n')
+        .filter((line) => !line.startsWith('Host:') && !line.startsWith('# Host'))
+        .join('\n')
+        .replace(/\n{2,}/g, '\n\n')
+        .trimEnd(),
+  },
 }
 
 
